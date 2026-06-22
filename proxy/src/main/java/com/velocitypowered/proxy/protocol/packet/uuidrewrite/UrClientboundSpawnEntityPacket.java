a33d6f1f0bd235fe7b47cdfb720ad2dac342f9a0
Fallen_Breath <nellaforax@outlook.com>
2026-06-23 02:57:01 +0800

    player uuid rewrite: mc26.2 support

--- proxy/src/main/java/com/velocitypowered/proxy/protocol/packet/uuidrewrite/UrClientboundSpawnEntityPacket.java
+++ proxy/src/main/java/com/velocitypowered/proxy/protocol/packet/uuidrewrite/UrClientboundSpawnEntityPacket.java
@@ -44,6 +44,7 @@ public class UrClientboundSpawnEntityPacket implements MinecraftPacket, PacketTo
   // https://github.com/Fallen-Breath/mc-registry-dump/tree/master/output, data["entity_type"]["minecraft:player"]
   // https://github.com/PrismarineJS/minecraft-data/blob/master/data/pc/1.21.4/entities.json
   private static final EntityTypeId[] PLAYER_ENTITY_TYPE_ID_MAPPINGS = new EntityTypeId[]{
+      new EntityTypeId(ProtocolVersion.MINECRAFT_26_2, 156),
       new EntityTypeId(ProtocolVersion.MINECRAFT_1_21_11, 155),
       new EntityTypeId(ProtocolVersion.MINECRAFT_1_21_9, 151),
       new EntityTypeId(ProtocolVersion.MINECRAFT_1_21_6, 149),
